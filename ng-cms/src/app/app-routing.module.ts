91e598d2641118f699c025dc786aec77b96b0e8f
laurafletcher <laura@fletchcom.net>
2020-11-20 16:52:07 -0600

    Generated and routed ArticleEditView

--- ng-cms/src/app/app-routing.module.ts
+++ ng-cms/src/app/app-routing.module.ts
@@ -4,13 +4,14 @@ import { NgModule }             from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 // 2. Import the UserComponent
-import { UsersComponent }   from './users/users.component';
 import { UserViewComponent }   from './user-view/user-view.component';
 import { UserCreateComponent }   from './user-create/user-create.component';
 import { UserEditComponent }   from './user-edit/user-edit.component';
 import { ArticlesComponent }   from './articles/articles.component';
 import { ArticleViewComponent }   from './article-view/article-view.component';
 import { ArticleCreateComponent }   from './article-create/article-create.component';
+import { ArticleEditComponent }   from './article-edit/article-edit.component';
+import { UsersComponent } from './users/users.component';
 
 // 3. Declare your routes
 const routes: Routes = [
@@ -21,7 +22,8 @@ const routes: Routes = [
   { path: 'users/edit/:id', component: UserEditComponent },
   { path: 'articles', component: ArticlesComponent },
   { path: 'articles/view/:id', component: ArticleViewComponent },
-  { path: 'articles/create', component: ArticleCreateComponent }
+  { path: 'articles/create', component: ArticleCreateComponent },
+  { path: 'articles/edit/:id', component: ArticleEditComponent }
 ];
 
 @NgModule({
